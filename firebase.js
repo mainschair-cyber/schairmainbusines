e057231fd87aea411c18cb68639b2fd62e0be6f8
mainschair-cyber <mainschair@gmail.com>
2026-08-06 07:17:40 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,24 @@
-
+<script type="module">
+‎  // Import the functions you need from the SDKs you need
+‎  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+‎  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-analytics.js";
+‎  // TODO: Add SDKs for Firebase products that you want to use
+‎  // https://firebase.google.com/docs/web/setup#available-libraries
+‎
+‎  // Your web app's Firebase configuration
+‎  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+‎  const firebaseConfig = {
+‎    apiKey: "AIzaSyCpCSJphFzYfghLGVR99RbYOVnNQfBhD1s",
+‎    authDomain: "schairmainbusines.firebaseapp.com",
+‎    projectId: "schairmainbusines",
+‎    storageBucket: "schairmainbusines.firebasestorage.app",
+‎    messagingSenderId: "540015695794",
+‎    appId: "1:540015695794:web:ef103b21429a76658b76ab",
+‎    measurementId: "G-RBRFPR02YH"
+‎  };
+‎
+‎  // Initialize Firebase
+‎  const app = initializeApp(firebaseConfig);
+‎  const analytics = getAnalytics(app);
+‎</script>
+‎
